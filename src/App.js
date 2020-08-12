a91953fb414d9a6aaf205f2199130adfd98f1ca8
Your Name <you@example.com>
2020-08-12 14:25:49 +0900

    defaultprops 활용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hello from './Hello';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <Hello name="dani" color="red"/>
+    <Hello color="blue"/>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,20 @@
 //props를 활용하는 기본 방법
+//default 활용
+
 
 import React from 'react';
 
-function Hello(props){
-    return <div style={{color:props.color}}> 안녕하세요{props.name}</div>
+// function Hello(props){
+//     return <div style={{color:props.color}}> 안녕하세요{props.name}</div>
+// }
+
+
+function Hello({color, name}){
+    return <div style={{color}}> 안녕하세요{name}</div>
+}
+
+Hello.defaultProps = {
+    name:"대전"
 }
 
 export default Hello;
