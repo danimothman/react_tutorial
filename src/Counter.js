af0988882c7f7e7a77d58f0f1504a5731b695610
Your Name <you@example.com>
2020-08-13 15:33:25 +0900

    App useReducer 활용 useRemove_미완부분있음

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,36 @@
 //userState활용한 Component 관리
-import React, {useState} from 'react'
+import React, {useReducer} from 'react'
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state +1;
+        case 'DECREMENT':
+            return state -1;
+        default:
+            return state
+    }
+}
+
+
 
 function Counter(){
     //setter 함수로 setNumber를 이용
+    // const [number, setNumber] = useState(1)
 
-    const [number, setNumber] = useState(1)
-
+    const [number, dispatch] = useReducer(reducer, 0)
     const onIncrease =()=>{
-        console.log(+1)
-        // setNumber(number+1)
-        setNumber(preNumber=>preNumber+1)
+        // console.log(+1)
+        // // setNumber(number+1)
+        // setNumber(preNumber=>preNumber+1)
+        dispatch({type:'INCREMENT'})
     }
 
     const onDecrease =()=>{
-        console.log(-1)
-        // setNumber(number-1)
-        setNumber(preNumber=>preNumber-1)
+        // console.log(-1)
+        // // setNumber(number-1)
+        // setNumber(preNumber=>preNumber-1)
+        dispatch({type:'DECREMENT'})
     }
 
 
--- src/UserList.js
+++ src/UserList.js
@@ -3,32 +3,6 @@ import React,{useEffect} from 'react'
 
 
 
-// function UserList(){
-// const users = [
-//     {
-//         id:1,
-//         username:"KIM",
-//         email:"1@naver.com"
-//     },
-//     {
-//         id:1,
-//         username:"SONG",
-//         email:"2@naver.com"
-//     },
-//     {
-//         id:1,
-//         username:"JAG",
-//         email:"3@naver.com"
-//     }
-// ]
-//     return (
-//         <div>
-//             <div><b>{users[0].username}: </b><span>{users[0].email}</span></div>
-//             <div><b>{users[1].username}: </b><span>{users[1].email}</span></div>
-//             <div><b>{users[2].username}: </b><span>{users[2].email}</span></div>
-//         </div>
-//     )
-// }
 //배열 항목 제거
 var User = ({user, onToggle, onRemove})=>{
     useEffect(()=>{
