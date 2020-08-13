fd54a0401cc164bc075c1a5616cbb9099e80bb89
Your Name <you@example.com>
2020-08-13 11:53:49 +0900

    활성화 사용자 체크, useMemo로 최적화 기능 활용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef , useState } from 'react';
+import React, { useRef , useState, useMemo, useCallback } from 'react';
 import Hello from './Hello'
 import Wrapper from './Wrapper'
 import Counter from './Counter'
@@ -8,7 +8,11 @@ import CreateUser from './CreateUser'
 
 
 function App() {
- 
+  const countActiveUsers = (users)=>{
+    console.log('활성화 사용자 수를 체크 하는중')
+    return users.filter(user=>user.active).length;
+  }
+
   const [users , setUsers] = useState (
     [
       {
@@ -50,7 +54,7 @@ function App() {
       [name] :value
     });
   }
-  const onCreate =()=>{
+  const onCreate =useCallback(()=>{
     const user = {
       id:nextId.current,
        username,
@@ -63,7 +67,7 @@ function App() {
       email:''
     })
     nextId.current +=1
-  }
+  },[inputs])
 
   const onRemove = (id)=>{
       setUsers(users.filter(user=>user.id !== id))
@@ -74,6 +78,8 @@ function App() {
       user=> user.id === id? {...user , active: !user.active} : user
     ))
   }
+
+  const count =useMemo(()=> countActiveUsers(users), [users])
   return (
       <>
       <Wrapper>
@@ -84,6 +90,7 @@ function App() {
       <InputSample />
       <CreateUser user={username} useremail={email} onChange={onChangeInput}  onCreate={onCreate}/>
       <UserList  users={users} onRemove={onRemove} onToggle={onToggleFunc} />
+      <div>활성자 수 : {count}</div>
       </>
   );
 }
